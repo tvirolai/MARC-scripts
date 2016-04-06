63a0bc470f86b8c9c78c3a4bd459bff2380ce6b5
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2016-04-06 10:35:27 +0300

    getRecsFromList

---
+++ random-utilities/getRecsFromList.js
@@ -0,0 +1,64 @@
+/* jshint node: true */
+
+/* This script reads a list of Melinda-id's, fetches the records from Melinda API and 
+* writes them to a MARC 21 (Exchange format) batch file.
+* Arguments: 1) file to read (list of id's), 2) outputfile for fetched records. */
+
+'use strict';
+
+const fs = require('fs');
+const request = require('request');
+const readline = require('readline');
+const Serializers = require('marc-record-serializers');
+const toISO2709 = Serializers.ISO2709.toISO2709;
+const fromXML = Serializers.MARCXML.fromMARCXML;
+const apiEndpoint = "http://melinda.kansalliskirjasto.fi/API/v1/bib/";
+
+let inputFile = "";
+let outputFile = "";
+let idList = [];
+
+if (process.argv.length !== 4) {
+  console.log('Usage: node getRecsFromList.js inputfile outputfile');
+  process.exit();
+} else {
+  inputFile = process.argv[2];
+  outputFile = process.argv[3];
+}
+
+let out = fs.createWriteStream(outputFile);
+
+const rl = readline.createInterface({
+  input: fs.createReadStream(inputFile)
+});
+
+let totalRecs = 0;
+let readRecs = 0;
+
+rl.on("line", line => {
+  if (line.length === 9) {
+    idList.push(line);
+  }
+}).on("close", () => {
+  totalRecs = idList.length;
+  getRecords(idList);
+});
+
+
+function getRecords(ids) {
+  let uri = apiEndpoint + ids.pop();
+  request(uri, (err, res, body) => {
+    readRecs++;
+    console.log("Read " + readRecs + " / " + totalRecs + " records.");
+    let record = fromXML(body);
+    //console.log(record);
+    out.write(toISO2709(record));
+    if (ids.length > 0) {
+      setTimeout( () => {
+        getRecords(ids);
+      }, 500);
+    } else {
+      console.log("Done.");
+    }
+  });
+}

b7c07ec8fc1029561d6b06554139fb220b6ad723
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2016-09-15 10:17:44 +0300

    Download-dump

---
+++ AlephSequential/download-dump/src/download_dump/core.clj
@@ -0,0 +1,41 @@
+(ns download-dump.core
+  (:require [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]])
+  (:gen-class))
+
+; The program downloads Melinda dump files from the replication server,
+; streaming them into a single, humongous Aleph sequential file.
+
+(defn URLs
+  [urlrange]
+  (map 
+    #(str "http://replikointi-kk.lib.helsinki.fi/index/alina" (if (< % 10) 0) % ".seq") 
+       (range urlrange)))
+
+(defn delete-existing
+  [file]
+  (if (.exists (io/as-file file))
+    (do
+      (println (str "File " file " exists, deleting it."))
+      (io/delete-file file))
+    (println (str "Created file " file "."))))
+
+(defn download [uri file]
+  (try
+    (with-open [in (io/input-stream uri) 
+                out (io/output-stream file :append true)]
+      (println (str "Downloading file " uri "."))
+      (io/copy in out))
+  (catch java.io.FileNotFoundException e
+    (println (str "Not found: " uri)))))
+
+(def cli-options
+  [["-f" "--file FILE" "Input file"]])
+
+(defn -main [& args]
+  (let [parsed-args (parse-opts args cli-options)
+        outputfile (:file (:options parsed-args))]
+    (do
+      (delete-existing outputfile)
+      (doseq [x (URLs 15)]
+       (download x outputfile)))))
---
+++ AlephSequential/download-dump/test/download_dump/core_test.clj
@@ -0,0 +1,7 @@
+(ns download-dump.core-test
+  (:require [clojure.test :refer :all]
+            [download-dump.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
